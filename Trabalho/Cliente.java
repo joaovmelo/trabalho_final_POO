4c2e1405c3a9d1d155a997b3d69be31bfc42c578
João Victor <joaovictormelo@ufu.br>
2025-05-07 23:01:42 -0300

    v.06

--- Trabalho/Cliente.java
+++ Trabalho/Cliente.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Cliente extends Pessoa {
+public class Cliente extends Pessoa implements Imprimivel{
     Scanner sc = new Scanner(System.in);
     private List<Conta> contas;
 
@@ -25,13 +25,17 @@ public class Cliente extends Pessoa {
         return contas;
     }
 
-    /*public String toString(Cliente cliente, Endereco endereco){
+    public String toString(Cliente cliente, Endereco endereco){
         return "Nome: " + cliente.getNome() + 
         "\nCPF: " + validadacaoCPF.imprimeCPF(cliente.getCpf()) + 
         "\nEstado Civil: " + cliente.getEstadoCivil() + 
         "\nEscolaridade: " + cliente.getEscolaridade() + 
         "\nEndereço: " + Endereco.getEndereco(endereco).getRua() + ", " + Endereco.getEndereco(endereco).getBairro() + ", " + Endereco.getEndereco(endereco).getCidade() + 
         "\nData de nascimento: " + cliente.getDataNascimento();
-    }*/
+    }
 
+    @Override
+    public void imprimirDados() {
+        System.out.println(this.toString());
+    }
 }
